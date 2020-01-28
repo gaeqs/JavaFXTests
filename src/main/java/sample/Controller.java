2ae3702d16806c28054ec315972ec88224fd0733
Gael Rial <gael.rial.costas@gmail.com>
2020-01-28 02:04:23 +0100

    Added RichTextFX.

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.SplitPane;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import sample.filetab.FileTab;
@@ -11,24 +13,30 @@ import sample.sidebar.SidebarController;
 public class Controller {
 
 	@FXML
-	private AnchorPane basePane;
+	private MenuBar mainMenu;
 
 	@FXML
 	private SplitPane mainPane;
 
 	@FXML
-	private VBox leftPaneMenu;
+	private VBox leftSidebar;
 
 	@FXML
 	private TabPane filesTabPane;
 
 	public void initialize() {
-		SidebarController controller = new SidebarController(leftPaneMenu, mainPane, true);
+		SidebarController controller = new SidebarController(leftSidebar, mainPane, true);
 		controller.addPane("Project", new AnchorPane());
 		controller.addPane("Structure", new AnchorPane());
 		controller.addPane("Favorites", new AnchorPane());
 
 		filesTabPane.getTabs().add(new FileTab("New file"));
+
+		MenuItem item = new MenuItem("Patata");
+		item.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/index.png"))));
+		item.setAccelerator(KeyCombination.valueOf("Ctrl+C"));
+
+		mainMenu.getMenus().add(new Menu("Test", null, item));
 	}
 
 
--- src/main/java/sample/filetab/FileTab.java
+++ src/main/java/sample/filetab/FileTab.java
@@ -1,12 +1,37 @@
 package sample.filetab;
 
 import javafx.scene.control.Tab;
-import javafx.scene.control.TextArea;
+import org.fxmisc.richtext.CodeArea;
+import org.fxmisc.richtext.model.StyleSpans;
+import org.fxmisc.richtext.model.StyleSpansBuilder;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Collections;
 
 public class FileTab extends Tab {
 
 	public FileTab(String name) {
-		super(name, new TextArea());
+		super(name);
+		CodeArea codeArea = new CodeArea();
+
+		//codeArea.setParagraphGraphicFactory(LineNumberFactory.get(codeArea));
+
+		StyleSpansBuilder<Collection<String>> spansBuilder = new StyleSpansBuilder<>();
+		spansBuilder.add(Collections.singleton("normal"), 20);
+		spansBuilder.add(Collections.singleton("patata"), 20);
+
+		StyleSpans<Collection<String>> spans = spansBuilder.create();
+
+		codeArea.multiPlainChanges()
+				.successionEnds(Duration.ofMillis(5)).subscribe(ignore -> codeArea.setStyleSpans(0, spans));
+
+		codeArea.getStyleClass().add("text-area");
+		codeArea.getStylesheets().add(getClass().getResource("/test.css").toString());
+
+		setContent(codeArea);
+
 	}
+
 
 }
